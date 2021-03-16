8ef978e2fbc966118b3da00cf2d4c28358172dc5
Dima <dg646726@gmail.com>
2021-03-16 20:01:56 +0200

    Add project files.

---
+++ Network.Prog_Дз_3_Server/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using System.Xml.Serialization;
+
+namespace Network.Prog_Дз_3_Server
+{
+    class Program
+    {
+        static int port = 8080;
+        static void Main(string[] args)
+        {
+            IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+
+                listenSocket.Bind(ipPoint);
+
+
+                listenSocket.Listen(10);
+
+                Console.WriteLine("Server started! Waiting for connection...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    Task.Run(() => SendScreenshots(handler));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void SendScreenshots(Socket handler)
+        {
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            byte[] data = new byte[256];
+            while (true)
+            {
+
+                do
+                {
+                    bytes = handler.Receive(data);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (handler.Available > 0);
+                Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                Thread.Sleep(int.Parse(builder.ToString()) * 1000);
+
+                data = PrintScreen();
+                handler.Send(data);
+            }
+
+
+        }
+        static byte[] PrintScreen()
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            using (var memoryStream = new MemoryStream())
+            {
+                printscreen.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return memoryStream.ToArray();
+            }
+
+        }
+    }
+}
---
+++ Network.Prog_Дз_3_User/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Network.Prog_Дз_3_User
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isStarted = false;
+        int secondsDelay = 2;
+        static int port = 8080;
+        IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            isStarted = true;
+            Task.Run(() => GettingScreenshots());
+        }
+        private void GettingScreenshots()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(iPAddress, port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(ipPoint);
+
+            try
+            {
+                while (true)
+                {
+                    if (isStarted == false) return;
+                    byte[] data = Encoding.Unicode.GetBytes(secondsDelay.ToString());
+                    socket.Send(data);
+
+                    data = new byte[256];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                    }
+                    while (socket.Available > 0);
+
+
+                    Application.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        var bitmapImage = BitmaSourceFromByteArray(data);
+                        image.Source = bitmapImage;
+
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                socket?.Shutdown(SocketShutdown.Both);
+                socket?.Close();
+            }
+
+        }
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            isStarted = false;
+        }
+
+        private void FirstRadionButton_Checked(object sender, RoutedEventArgs e)
+        {
+            secondsDelay = Convert.ToInt32((sender as RadioButton).Content);
+        }
+        public static BitmapSource BitmaSourceFromByteArray(byte[] buffer)
+        {
+            using (var stream = new MemoryStream(buffer))
+            {
+                return BitmapFrame.Create(
+                    stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            }
+        }
+    }
+}
